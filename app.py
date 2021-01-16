0d031b0a571ded073be128ca4a05168eeb56734e
Adham Nour <adhamnourelwaffaa@gmail.com>
2021-01-16 14:19:56 +0200

    finished the totorial

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-@app.route("/")
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    content = db.Column(db.String(255), nullable=False)
+    date_created = db.Column(db.DateTime,default=datetime.utcnow())
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route("/",methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        newTask = Todo(content = task_content)
+        try:
+            db.session.add(newTask)
+            db.session.commit()
+            return redirect('/')
+        except Exception as e:
+            raise 'error occured'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html',tasks=tasks)
 
 if __name__ == "__main__":
     app.run(debug=True)
